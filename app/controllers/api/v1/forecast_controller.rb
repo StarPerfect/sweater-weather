b06d4259085526f0add4e898cad143f895380625
Corina Allen <StarPerfect@gmail.com>
2019-11-03 16:38:13 -0700

    Refactor ForecastController to run through ForecastFacade - still need serializer

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,22 +1,10 @@
 class Api::V1::ForecastController < ApplicationController
   def index
     geocoding = GoogleApiService.new(location_params)
-
     darksky = DarkskyApiService.new(geocoding.latitude, geocoding.longitude)
     dark_parsed = darksky.response
-
-    current = CurrentForecast.new(@country_location, dark_parsed)
-
-    daily = dark_parsed[:daily][:data].map do |day|
-      DailyForecast.new(day)
-    end.take(5)
-
-    hourly = dark_parsed[:hourly][:data].map do |hour|
-      HourlyForecast.new(hour)
-    end.take(8)
-    
-    full_forecast = ForecastFacade.new(current, daily, hourly)
-    render json: ForecastSerializer.new(full_forecast)
+    full_forecast = ForecastFacade.new(geocoding.country_location, dark_parsed)
+    render json: SERIALIZER.new(full_forecast)
   end
 
   private
