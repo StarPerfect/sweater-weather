b1e9e005e1f1ab9cd3b427e950e0bb768eadd0ef
Corina Allen <StarPerfect@gmail.com>
2019-11-03 09:37:20 -0700

    Update RailsHelper to exclude Jobs and Channels

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,8 @@ require 'simplecov'
 SimpleCov.start 'rails' do
   add_filter '/bin/'
   add_filter '/db/'
+  add_filter '/app/jobs'
+  add_filter '/app/channels'
   add_filter '/spec/' # for rspec
 end
 
