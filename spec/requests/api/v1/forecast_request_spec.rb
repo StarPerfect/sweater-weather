f2ca1b3ff3244d2cbd15b21d5e3ee52e83a690c0
Corina Allen <StarPerfect@gmail.com>
2019-11-03 19:48:16 -0700

    Added more robust testing

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -6,5 +6,14 @@ describe 'Forecast API Endpoint' do
     get '/api/v1/forecast?location=denver,co'
 
     expect(response).to be_successful
+
+    parsed_data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response.content_type).to eq('application/json')
+    expect(parsed_data.class).to eq(Hash)
+    expect(parsed_data[:data][:attributes].count).to eq(3)
+    expect(parsed_data[:data][:attributes].keys).to eq([:current, :daily, :hourly])
+    expect(parsed_data[:data][:attributes][:daily].count).to eq(5)
+    expect(parsed_data[:data][:attributes][:hourly].count).to eq(8)
   end
 end
