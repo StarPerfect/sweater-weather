c738c70964b944bb8a7358ec0bea73ffdcb42316
Corina Allen <StarPerfect@gmail.com>
2019-11-04 11:43:13 -0700

    Clean up services after refactor to only reference one instance of the Google service across controllers

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -1,9 +1,8 @@
 class Api::V1::AntipodeController < ApplicationController
   def index
-    geocoding = GoogleApiService.new
-    amypode = AmypodeApiService.new(geocoding.latitude({address: search_location}), geocoding.longitude({address: search_location}))
-    find_city = GoogleApiService.new
-    antipode_city = find_city.get_city({ latlng: "#{amypode.lat},#{amypode.long}" })
+    google = GoogleApiService.new
+    amypode = AmypodeApiService.new(google.latitude({address: search_location}), google.longitude({address: search_location}))
+    antipode_city = google.get_city({ latlng: "#{amypode.lat},#{amypode.long}" })
     darksky = DarkskyApiService.new(amypode.lat, amypode.long)
     antipode = AntipodeFacade.new(search_location, antipode_city, darksky.response)
     render json: AntipodeSerializer.new(antipode)
--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,8 +2,7 @@ class Api::V1::ForecastController < ApplicationController
   def index
     geocoding = GoogleApiService.new
     darksky = DarkskyApiService.new(geocoding.latitude({address: location_params}), geocoding.longitude({address: location_params}))
-    dark_parsed = darksky.response
-    full_forecast = ForecastFacade.new(geocoding.country_location({address: location_params}), dark_parsed)
+    full_forecast = ForecastFacade.new(geocoding.country_location({address: location_params}), darksky.response)
     render json: ForecastSerializer.new(full_forecast)
   end
 
