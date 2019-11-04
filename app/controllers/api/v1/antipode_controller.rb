f2097c0385fcb97f3b4a30af4da42a4ac41d63a6
Corina Allen <StarPerfect@gmail.com>
2019-11-04 10:03:41 -0700

    Update AntipodeController with Reverse Geocoding service before pulling out the required data from the parsed JSON

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -3,9 +3,10 @@ class Api::V1::AntipodeController < ApplicationController
     geocoding = GoogleApiService.new(search_location)
 
     amypode = AmypodeApiService.new(geocoding.latitude, geocoding.longitude)
+    amypode.get_antipode
 
-    
-
+    find_city = GoogleReverseGeocodingService.new
+    antipode = find_city.get_city(amypode.get_antipode[:data][:attributes][:lat], amypode.get_antipode[:data][:attributes][:long])
     binding.pry
 
   end
