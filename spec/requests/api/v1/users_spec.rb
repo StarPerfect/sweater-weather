5479aa37516c0532fe5f44d1f3eacdf5be7b0919
Corina Allen <StarPerfect@gmail.com>
2019-11-05 16:58:36 -0700

    Update User Request Spec with sad path and more robust tests

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -14,8 +14,18 @@ RSpec.describe "Users", type: :request do
 
       expect(response.content_type).to eq('application/json')
       expect(response).to have_http_status(201)
+      expect(parsed_data[:data][:attributes].keys).to eq([:api_key])
+    end
+
+    it 'send error if invalid credentials are sent in post' do
+      post '/api/v1/users', params: { "email": "whatever@example.com",
+                                      "password": "password",
+                                      "password_confirmation": "wrong"
+                                    }
+
+      expect(response).to_not be_successful
       expect(response.content_type).to eq('application/json')
-      expect(user_data[:data][:attributes].keys).to eq([:api_key])
+      expect(response).to have_http_status(418)
     end
   end
 end
