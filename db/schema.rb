63224608c3e5b400a8707177414f5031da7c4a27
Corina Allen <StarPerfect@gmail.com>
2019-11-05 16:58:09 -0700

    Had to revert and redo some db migrations to correct the User model and creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_165827) do
+ActiveRecord::Schema.define(version: 2019_11_05_234919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
