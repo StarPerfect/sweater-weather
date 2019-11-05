3539280e347db2a12dc1220c4474f63aabadd5f9
Corina Allen <StarPerfect@gmail.com>
2019-11-05 09:18:20 -0700

    Update Routes with Users#show path

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       resources :forecast, only: [:index]
       resources :antipode, only: [:index]
       resources :backgrounds, only: [:index]
+      post 'users', to: 'users#show'
     end
   end
 end
