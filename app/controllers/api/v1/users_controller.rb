1210beef3a2121f384d310902a795b7681d833a8
Corina Allen <StarPerfect@gmail.com>
2019-11-05 15:13:33 -0700

    Create UsersController for creating user accounts via post endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,18 +1,16 @@
 class Api::V1::UsersController < ApplicationController
-  # POST /users
   def create
     @user = User.new(user_params)
-binding.pry
-    if @user.save
-      render json: @user, status: :created, location: @user
+    if user_params[:password] == user_params[:password_confirmation] && @user.save
+      render json: UserSerializer.new(@user), status: 201
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: 'Invalid email or password', status: 418
     end
   end
 
   private
 
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
+      params.permit(:email, :password, :password_confirmation)
     end
 end
