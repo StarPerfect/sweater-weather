a513c64fb3fe77929d0679f2fea948813e7f8d57
Corina Allen <StarPerfect@gmail.com>
2019-11-05 17:00:20 -0700

    Clean up Users#create with creating User if params correct or send a 418 if invalid

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
-    if user_params[:password] == user_params[:password_confirmation] && @user.save
+    if @user.save
       render json: UserSerializer.new(@user), status: 201
     else
       render json: 'Invalid email or password', status: 418
