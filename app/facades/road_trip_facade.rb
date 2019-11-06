606a8b8c7929711e25107f2ed3a59e1c64b79ea4
Corina Allen <StarPerfect@gmail.com>
2019-11-06 08:39:38 -0700

    Complete RoadTripFacade for compiling information for roadtrip endpoint

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,4 +1,6 @@
 class RoadTripFacade
+  attr_reader :id, :origin, :destination
+
   def initialize(origin, destination)
     @id = 27
     @origin = origin
@@ -11,6 +13,20 @@ class RoadTripFacade
   end
 
   def arrival_forecast
-    
+    darksky = DarkskyApiService.new(destination_lat, destination_long)
+    arrival_time = Time.now.to_i + travel_time_in_minutes
+    AntipodeForecast.new(darksky.future(arrival_time))
+  end
+
+  protected
+
+  def destination_lat
+    geocoding = GoogleApiService.new({address: @destination}, 'geocode')
+    geocoding.latitude
+  end
+
+  def destination_long
+    geocoding = GoogleApiService.new({address: @destination}, 'geocode')
+    geocoding.longitude
   end
 end
